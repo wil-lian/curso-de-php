1e5d70127f7aaab608d4e1d87d1ad1a9f150ab3c
will <guillermototi11@gmail.com>
2023-03-24 14:38:50 -0400

    terminando subida de archivos al backend

--- 3PHP Avanzado/5Carga_de_archivos PHP/upload.php
+++ 3PHP Avanzado/5Carga_de_archivos PHP/upload.php
@@ -1,16 +1,16 @@
 <?php
-$target_dir = "5carga_de_archivosPHP/";
+$target_dir = "uploads/upload.php";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 //Comprobar si el archivo de imagen es una imagen real o una imagen falsa
 if (isset($_POST["submit"])) {
-    $check = getimagesize(($_FILES["fileToUpload"]["tmp_name"]));
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if ($check !== false) {
         echo "El archivo es una imagen - " . $check["mime"] . ".";
         $uploadOk = 1;
-    } else if (file_exists($target_file)) {
+    } else {
         echo "El archivo no es una imagen.";
         $uploadOk = 0;
     }
@@ -40,9 +40,8 @@ if (
 // Comprobar si $uploadOk se establece en 0 por un error
 if ($uploadOk == 0) {
     echo "Lo sentimos, su archivo no fue subido";
-}
-// si todo está bien, intente cargar el archivo
-else {
+    // si todo está bien, intente cargar el archivo
+} else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "Este archivo " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " ha sido subido";
     } else {
