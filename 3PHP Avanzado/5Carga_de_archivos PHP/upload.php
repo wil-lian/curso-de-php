cd174b56f0a846e11086f4d25ee007d8ec815a5d
will <guillermototi11@gmail.com>
2023-03-24 13:35:40 -0400

    cargar archivos al backend

--- 3PHP Avanzado/5Carga_de_archivos PHP/upload.php
+++ 3PHP Avanzado/5Carga_de_archivos PHP/upload.php
@@ -3,7 +3,8 @@ $target_dir = "5carga_de_archivosPHP/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-// Comprobar si el archivo de imagen es una imagen real o una imagen falsa
+
+//Comprobar si el archivo de imagen es una imagen real o una imagen falsa
 if (isset($_POST["submit"])) {
     $check = getimagesize(($_FILES["fileToUpload"]["tmp_name"]));
     if ($check !== false) {
@@ -14,10 +15,37 @@ if (isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-    /*  Script PHP explicado:
-        $target_dir = "uploads/": especifica el directorio donde se colocará el archivo
-        $target_file especifica la ruta del archivo que se cargará
-        $uploadOk=1 aún no se usa (se usará más adelante)
-        $imageFileType contiene la extensión de archivo del archivo (en minúsculas)
-        A continuación, compruebe si el archivo de imagen es una imagen real o una imagen falsa
-    */
+
+// Comprobar si el archivo ya existe
+if (file_exists($target_file)) {
+    echo "El archivo ya existe";
+    $uploadOk = 0;
+}
+
+// Comprobar el tamaño del archivo
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Lo sentimos su archivo es demasiado grande";
+    $uploadOk = 0;
+}
+
+// Permitir ciertos formatos de archivo
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+) {
+    echo "Lo sentimos solo se permite archivos jpg, png, jpeg, gif";
+    $uploadOk = 0;
+}
+
+// Comprobar si $uploadOk se establece en 0 por un error
+if ($uploadOk == 0) {
+    echo "Lo sentimos, su archivo no fue subido";
+}
+// si todo está bien, intente cargar el archivo
+else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "Este archivo " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " ha sido subido";
+    } else {
+        echo "Lo sentimos hubo un error al subir el archivo";
+    }
+}
